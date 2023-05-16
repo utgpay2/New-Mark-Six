5cab4c5aea09a6424bfd6355eb7a40bb575cb220
一休 <hongchengwei1986@gamil.com>
2023-05-16 12:54:54 +0800

    基础版本

--- zlt-business/backend-server/src/main/java/com/central/backend/controller/KpnSiteAdvertiseController.java
+++ zlt-business/backend-server/src/main/java/com/central/backend/controller/KpnSiteAdvertiseController.java
@@ -1,104 +1,104 @@
-package com.central.backend.controller;
-
-import cn.hutool.core.util.ObjectUtil;
-import com.central.backend.co.KpnSiteAdvertiseCo;
-import com.central.backend.co.KpnSiteAdvertiseUpdateCo;
-import com.central.backend.service.IKpnSiteAdvertiseService;
-import com.central.common.annotation.LoginUser;
-import com.central.common.constant.MarksixConstants;
-import com.central.common.model.*;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-@Slf4j
-@RestController
-@Api(tags = "广告配置api")
-@Validated
-@RefreshScope
-@RequestMapping("/advertise")
-public class KpnSiteAdvertiseController {
-
-    @Autowired
-    private IKpnSiteAdvertiseService advertiseService;
-
-
-    @Value("${zlt.minio.externalEndpoint}")
-    private String externalEndpoint;
-
-    /* 查询广告列表
-     * @Author: Lulu
-     * @Date: 2023/2/3
-     */
-    @ApiOperation("查询广告列表")
-    @ResponseBody
-    @GetMapping("/findAdvertiseList")
-    public Result<PageResult<KpnSiteAdvertise>> findAdvertiseList(@ModelAttribute KpnSiteAdvertiseCo params) {
-        PageResult<KpnSiteAdvertise> kpnSiteAdvertiseList = advertiseService.findAdvertiseList(params);
-        kpnSiteAdvertiseList.getData().stream().forEach(info->{
-            if (info.getUrl()!=null){
-                info.setUrl(externalEndpoint+ MarksixConstants.Symbol.FORWARD_SLASH +info.getUrl());
-            }
-        });
-        return Result.succeed(kpnSiteAdvertiseList);
-    }
-
-
-    @ApiOperation(value = "新增or更新广告")
-    @PostMapping(value = "/saveOrUpdateAdvertise")
-    public Result saveOrUpdateAdvertise(@RequestBody  KpnSiteAdvertise advertise, @LoginUser SysUser sysUser)  {
-        if (sysUser!=null){
-            if (advertise.getId() == null) {
-                advertise.setUpdateBy(sysUser.getUsername());
-                advertise.setCreateBy(sysUser.getUsername());
-            }else {
-                advertise.setUpdateBy(sysUser.getUsername());
-            }
-        }
-        if (ObjectUtil.isEmpty(advertise.getNameZh()) || ObjectUtil.isEmpty(advertise.getNameEn()) || ObjectUtil.isEmpty(advertise.getNameKh())) {
-            return Result.failed("广告名称不能为空");
-        }
-        if (ObjectUtil.isEmpty(advertise.getDevice())) {
-            return Result.failed("投放平台不能为空");
-        }
-        if (ObjectUtil.isEmpty(advertise.getPosition())) {
-            return Result.failed("广告位置不能为空");
-        }
-        if (ObjectUtil.isEmpty(advertise.getSort())) {
-            return Result.failed("排序不能为空");
-        }
-        if (ObjectUtil.isEmpty(advertise.getUrl())) {
-            return Result.failed("广告图片不能为空");
-        }
-        if (ObjectUtil.isEmpty(advertise.getStartTime()) || ObjectUtil.isEmpty(advertise.getEndTime())) {
-            return Result.failed("有效时间不能为空");
-        }
-        return  advertiseService.saveOrUpdateAdvertise(advertise);
-    }
-
-    @ApiOperation(value = "修改广告状态")
-    @GetMapping("/updateEnabledAdvertise")
-    public Result updateEnabledAdvertise(@Valid @ModelAttribute KpnSiteAdvertiseUpdateCo params, @LoginUser SysUser sysUser) {
-        if (sysUser!=null) {
-            params.setUpdateBy(sysUser.getUsername());
-        }
-        //Result result = advertiseService.updateEnabledAdvertise(params);
-        return Result.succeed("删除成功") ;
-    }
-
-    @ApiOperation("删除广告")
-    @DeleteMapping(value = "/deleteAdvertiseId/{id}")
-    public Result deleteAdvertiseId(@PathVariable Long id) {
-        boolean b = advertiseService.deleteAdvertiseId(id);
-        return b ? Result.succeed("删除成功") : Result.failed("删除失败");
-    }
-
-
-}
+//package com.central.backend.controller;
+//
+//import cn.hutool.core.util.ObjectUtil;
+//import com.central.backend.co.KpnSiteAdvertiseCo;
+//import com.central.backend.co.KpnSiteAdvertiseUpdateCo;
+//import com.central.backend.service.IKpnSiteAdvertiseService;
+//import com.central.common.annotation.LoginUser;
+//import com.central.common.constant.MarksixConstants;
+//import com.central.common.model.*;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//
+//@Slf4j
+//@RestController
+//@Api(tags = "广告配置api")
+//@Validated
+//@RefreshScope
+//@RequestMapping("/advertise")
+//public class KpnSiteAdvertiseController {
+//
+//    @Autowired
+//    private IKpnSiteAdvertiseService advertiseService;
+//
+//
+//    @Value("${zlt.minio.externalEndpoint}")
+//    private String externalEndpoint;
+//
+//    /* 查询广告列表
+//     * @Author: Lulu
+//     * @Date: 2023/2/3
+//     */
+//    @ApiOperation("查询广告列表")
+//    @ResponseBody
+//    @GetMapping("/findAdvertiseList")
+//    public Result<PageResult<KpnSiteAdvertise>> findAdvertiseList(@ModelAttribute KpnSiteAdvertiseCo params) {
+//        PageResult<KpnSiteAdvertise> kpnSiteAdvertiseList = advertiseService.findAdvertiseList(params);
+//        kpnSiteAdvertiseList.getData().stream().forEach(info->{
+//            if (info.getUrl()!=null){
+//                info.setUrl(externalEndpoint+ MarksixConstants.Symbol.FORWARD_SLASH +info.getUrl());
+//            }
+//        });
+//        return Result.succeed(kpnSiteAdvertiseList);
+//    }
+//
+//
+//    @ApiOperation(value = "新增or更新广告")
+//    @PostMapping(value = "/saveOrUpdateAdvertise")
+//    public Result saveOrUpdateAdvertise(@RequestBody  KpnSiteAdvertise advertise, @LoginUser SysUser sysUser)  {
+//        if (sysUser!=null){
+//            if (advertise.getId() == null) {
+//                advertise.setUpdateBy(sysUser.getUsername());
+//                advertise.setCreateBy(sysUser.getUsername());
+//            }else {
+//                advertise.setUpdateBy(sysUser.getUsername());
+//            }
+//        }
+//        if (ObjectUtil.isEmpty(advertise.getNameZh()) || ObjectUtil.isEmpty(advertise.getNameEn()) || ObjectUtil.isEmpty(advertise.getNameKh())) {
+//            return Result.failed("广告名称不能为空");
+//        }
+//        if (ObjectUtil.isEmpty(advertise.getDevice())) {
+//            return Result.failed("投放平台不能为空");
+//        }
+//        if (ObjectUtil.isEmpty(advertise.getPosition())) {
+//            return Result.failed("广告位置不能为空");
+//        }
+//        if (ObjectUtil.isEmpty(advertise.getSort())) {
+//            return Result.failed("排序不能为空");
+//        }
+//        if (ObjectUtil.isEmpty(advertise.getUrl())) {
+//            return Result.failed("广告图片不能为空");
+//        }
+//        if (ObjectUtil.isEmpty(advertise.getStartTime()) || ObjectUtil.isEmpty(advertise.getEndTime())) {
+//            return Result.failed("有效时间不能为空");
+//        }
+//        return  advertiseService.saveOrUpdateAdvertise(advertise);
+//    }
+//
+//    @ApiOperation(value = "修改广告状态")
+//    @GetMapping("/updateEnabledAdvertise")
+//    public Result updateEnabledAdvertise(@Valid @ModelAttribute KpnSiteAdvertiseUpdateCo params, @LoginUser SysUser sysUser) {
+//        if (sysUser!=null) {
+//            params.setUpdateBy(sysUser.getUsername());
+//        }
+//        //Result result = advertiseService.updateEnabledAdvertise(params);
+//        return Result.succeed("删除成功") ;
+//    }
+//
+//    @ApiOperation("删除广告")
+//    @DeleteMapping(value = "/deleteAdvertiseId/{id}")
+//    public Result deleteAdvertiseId(@PathVariable Long id) {
+//        boolean b = advertiseService.deleteAdvertiseId(id);
+//        return b ? Result.succeed("删除成功") : Result.failed("删除失败");
+//    }
+//
+//
+//}
--- zlt-business/backend-server/src/main/java/com/central/backend/controller/KpnSitePlatformController.java
+++ zlt-business/backend-server/src/main/java/com/central/backend/controller/KpnSitePlatformController.java
@@ -1,73 +1,73 @@
-package com.central.backend.controller;
-
-import cn.hutool.core.util.ObjectUtil;
-import com.central.backend.service.IAsyncService;
-import com.central.backend.service.IKpnSitePlatformService;
-import com.central.common.annotation.LoginUser;
-import com.central.common.model.KpnSitePlatform;
-import com.central.common.model.Result;
-import com.central.common.model.SysUser;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-@Slf4j
-@RestController
-@Api(tags = "平台配置api")
-@Validated
-@RequestMapping("/platform")
-public class KpnSitePlatformController {
-
-    @Autowired
-    private IKpnSitePlatformService sitePlatformService;
-
-    @Autowired
-    private IAsyncService asyncService;
-
-
-    /* 查询平台配置
-     * @Author: Lulu
-     * @Date: 2023/2/24
-     */
-    @ApiOperation("查询平台配置")
-    @ResponseBody
-    @GetMapping("/findPlatformInfo")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "siteId", value = "站点id", required = true, dataType = "Long"),
-    })
-    public Result<KpnSitePlatform> findPlatformInfo(@RequestParam("siteId") Long siteId) {
-        KpnSitePlatform promotion = sitePlatformService.findPromotionInfo(siteId);
-        return Result.succeed(promotion);
-    }
-
-
-
-
-    @ApiOperation(value = "保存配置")
-    @PostMapping("/saveOrUpdatePlatform")
-    public Result saveOrUpdatePlatform(@RequestBody KpnSitePlatform info, @LoginUser SysUser sysUser) {
-        if (sysUser!=null) {
-            if (info.getId() == null) {
-                info.setUpdateBy(sysUser.getUsername());
-                info.setCreateBy(sysUser.getUsername());
-            } else {
-                info.setUpdateBy(sysUser.getUsername());
-            }
-        }
-        if (ObjectUtil.isEmpty(info.getSiteId())) {
-            return Result.failed("站点id不能为空");
-        }
-        Boolean aBoolean = sitePlatformService.saveOrUpdatePlatform(info);
-
-        //add by year 删除站点平台配置缓存
-        if(aBoolean){
-            asyncService.deleteSitePlatformCache(info.getSiteId());
-        }
-        return aBoolean ? Result.succeed("操作成功") : Result.failed("操作失败");
-    }
-}
+//package com.central.backend.controller;
+//
+//import cn.hutool.core.util.ObjectUtil;
+//import com.central.backend.service.IAsyncService;
+//import com.central.backend.service.IKpnSitePlatformService;
+//import com.central.common.annotation.LoginUser;
+//import com.central.common.model.KpnSitePlatform;
+//import com.central.common.model.Result;
+//import com.central.common.model.SysUser;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiImplicitParam;
+//import io.swagger.annotations.ApiImplicitParams;
+//import io.swagger.annotations.ApiOperation;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Slf4j
+//@RestController
+//@Api(tags = "平台配置api")
+//@Validated
+//@RequestMapping("/platform")
+//public class KpnSitePlatformController {
+//
+//    @Autowired
+//    private IKpnSitePlatformService sitePlatformService;
+//
+//    @Autowired
+//    private IAsyncService asyncService;
+//
+//
+//    /* 查询平台配置
+//     * @Author: Lulu
+//     * @Date: 2023/2/24
+//     */
+//    @ApiOperation("查询平台配置")
+//    @ResponseBody
+//    @GetMapping("/findPlatformInfo")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "siteId", value = "站点id", required = true, dataType = "Long"),
+//    })
+//    public Result<KpnSitePlatform> findPlatformInfo(@RequestParam("siteId") Long siteId) {
+//        KpnSitePlatform promotion = sitePlatformService.findPromotionInfo(siteId);
+//        return Result.succeed(promotion);
+//    }
+//
+//
+//
+//
+//    @ApiOperation(value = "保存配置")
+//    @PostMapping("/saveOrUpdatePlatform")
+//    public Result saveOrUpdatePlatform(@RequestBody KpnSitePlatform info, @LoginUser SysUser sysUser) {
+//        if (sysUser!=null) {
+//            if (info.getId() == null) {
+//                info.setUpdateBy(sysUser.getUsername());
+//                info.setCreateBy(sysUser.getUsername());
+//            } else {
+//                info.setUpdateBy(sysUser.getUsername());
+//            }
+//        }
+//        if (ObjectUtil.isEmpty(info.getSiteId())) {
+//            return Result.failed("站点id不能为空");
+//        }
+//        Boolean aBoolean = sitePlatformService.saveOrUpdatePlatform(info);
+//
+//        //add by year 删除站点平台配置缓存
+//        if(aBoolean){
+//            asyncService.deleteSitePlatformCache(info.getSiteId());
+//        }
+//        return aBoolean ? Result.succeed("操作成功") : Result.failed("操作失败");
+//    }
+//}
